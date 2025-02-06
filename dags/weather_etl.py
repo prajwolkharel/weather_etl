705557af7909b982ffd37217fa35b71f6ff3094f
Prajwol Kharel <xyzkharelprajwol@gmail.com>
2025-02-06 15:44:49 +0545

    created transformation task

--- dags/weather_etl.py
+++ dags/weather_etl.py
@@ -35,11 +35,21 @@ with DAG(dag_id='weather_etl',
             # Make request via httphook
             response = http_hook(endpoint)
 
+            if response.status_code == 200:
+                  return response.json()
+            else:
+                  raise Exception("Failed to fetch weather dataL {response.status_code}")
 
-
-
-
-
-
-
-
+        @task
+        def transform_weather_data(weather_data):
+            """Transform the extracted weather data."""
+            current_weather = weather_data['current_weather']
+            transformed_data = {
+                  'latitude': LATITUDE,
+                  'longitude': LONGITUDE,
+                  'temperature': current_weather['temperature'],
+                  'windspeed': current_weather['windspeed'],
+                  'winddirection': current_weather['winddirection'],
+                  'weathercode': current_weather['weathercode']
+            }
+            return transformed_data
