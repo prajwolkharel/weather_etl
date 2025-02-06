71240d12f202ff6008c4c09cb6ba9822b8ec2d1b
Prajwol Kharel <xyzkharelprajwol@gmail.com>
2025-02-06 16:28:39 +0545

    updated

--- dags/weather_etl.py
+++ dags/weather_etl.py
@@ -1,46 +1,49 @@
 from airflow import DAG
-from airflow.providers.https.hooks.http import HttpHook
+from airflow.providers.http.hooks.http import HttpHook
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.decorators import task
 from airflow.utils.dates import days_ago
+import requests
+import json
 
-# Latitude and longitude for the desired location. eg, London
+# Latitude and longitude for the desired location (London in this case)
 LATITUDE = '51.5074'
 LONGITUDE = '-0.1278'
-POSTGRES_CONN_ID = 'postgres_default'
-API_CONN_ID = 'open_meteo_api'
+POSTGRES_CONN_ID='postgres_default'
+API_CONN_ID='open_meteo_api'
 
-# Default arguments for the DAG
-default_args = {
-    'owner': 'airflow',
-    'start_date': days_ago(1),
+default_args={
+    'owner':'airflow',
+    'start_date':days_ago(1)
 }
 
-# DAG
-with DAG(dag_id='weather_etl',
+## DAG
+with DAG(dag_id='weather_etl_pipeline',
          default_args=default_args,
          schedule_interval='@daily',
-         catchup=False) as dag:
+         catchup=False) as dags:
 
-    @task
+    @task()
     def extract_weather_data():
         """Extract weather data from Open-Meteo API using Airflow Connection."""
 
-        # HTTP Hook to get connection details from Airflow connection
-        http_hook = HttpHook(http_conn_id=API_CONN_ID, method='GET')
+        # Use HTTP Hook to get connection details from Airflow connection
 
-        # Build the API endpoint
-        endpoint = f'v1/forecast?latitude={LATITUDE}&longitude={LONGITUDE}&current_weather=true'
+        http_hook=HttpHook(http_conn_id=API_CONN_ID,method='GET')
 
-        # Make the request via http_hook
-        response = http_hook.run(endpoint)
+        ## Build the API endpoint
+        ## https://api.open-meteo.com/v1/forecast?latitude=51.5074&longitude=-0.1278&current_weather=true
+        endpoint=f'/v1/forecast?latitude={LATITUDE}&longitude={LONGITUDE}&current_weather=true'
+
+        ## Make the request via the HTTP Hook
+        response=http_hook.run(endpoint)
 
         if response.status_code == 200:
             return response.json()
         else:
             raise Exception(f"Failed to fetch weather data: {response.status_code}")
 
-    @task
+    @task()
     def transform_weather_data(weather_data):
         """Transform the extracted weather data."""
         current_weather = weather_data['current_weather']
@@ -54,14 +57,14 @@ with DAG(dag_id='weather_etl',
         }
         return transformed_data
 
-    @task
+    @task()
     def load_weather_data(transformed_data):
-        """Load transformed data into PostgreSQL"""
+        """Load transformed data into PostgreSQL."""
         pg_hook = PostgresHook(postgres_conn_id=POSTGRES_CONN_ID)
         conn = pg_hook.get_conn()
         cursor = conn.cursor()
 
-        # Create table if it does not exist
+        # Create table if it doesn't exist
         cursor.execute("""
         CREATE TABLE IF NOT EXISTS weather_data (
             latitude FLOAT,
@@ -74,7 +77,7 @@ with DAG(dag_id='weather_etl',
         );
         """)
 
-        # Insert transformed data into table
+        # Insert transformed data into the table
         cursor.execute("""
         INSERT INTO weather_data (latitude, longitude, temperature, windspeed, winddirection, weathercode)
         VALUES (%s, %s, %s, %s, %s, %s)
@@ -84,13 +87,13 @@ with DAG(dag_id='weather_etl',
             transformed_data['temperature'],
             transformed_data['windspeed'],
             transformed_data['winddirection'],
-            transformed_data['weathercode'],
+            transformed_data['weathercode']
         ))
 
         conn.commit()
         cursor.close()
 
-    # DAG Workflow - ETL Pipeline
-    weather_data = extract_weather_data()
-    transformed_data = transform_weather_data(weather_data)
+    ## DAG Worflow- ETL Pipeline
+    weather_data= extract_weather_data()
+    transformed_data=transform_weather_data(weather_data)
     load_weather_data(transformed_data)
