3bc2943c72bad8a4b6a26ed65e67ad85a2c844ab
Prajwol Kharel <xyzkharelprajwol@gmail.com>
2025-02-06 15:44:49 +0545

    created load task and defined workflow

--- dags/weather_etl.py
+++ dags/weather_etl.py
@@ -35,11 +35,62 @@ with DAG(dag_id='weather_etl',
             # Make request via httphook
             response = http_hook(endpoint)
 
+            if response.status_code == 200:
+                  return response.json()
+            else:
+                  raise Exception("Failed to fetch weather dataL {response.status_code}")
 
+        @task
+        def transform_weather_data(weather_data):
+            """Transform the extracted weather data."""
+            current_weather = weather_data['current_weather']
+            transformed_data = {
+                  'latitude': LATITUDE,
+                  'longitude': LONGITUDE,
+                  'temperature': current_weather['temperature'],
+                  'windspeed': current_weather['windspeed'],
+                  'winddirection': current_weather['winddirection'],
+                  'weathercode': current_weather['weathercode']
+            }
+            return transformed_data
 
+        @task
+        def load_weather_data(transformed_data):
+            """Load transformed data into PostgreSQL"""
+            pg_hook = PostgresHook(postgres_conn_id = POSTGRES_CONN_ID)
+            conn = pg_hook.get_conn()
+            cursor = conn.cursor()
 
+            # create table if it does not exist
+            cursor.execute("""
+            CREATE TABLE IF NOT EXISTS weather_data (
+                latitude FLOAT,
+                longitude FLOAT,
+                temperature FLOAT,
+                windspeed FLOAT,
+                winddirection FLOAT,
+                weathercode INT,
+                timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+            """)
 
+            # insert transformed data into table
+            cursor.execute("""
+            INSERT INTO weather_data (latitude, longitude, temperature, windspeed, windirection, weathercode)
+            VALUES (%s, %s, %s, %s, %s, %s)
+            """, (
+                transformed_data['latitude'],
+                transformed_data['longitude'],
+                transformed_data['temperature'],
+                transformed_data['windspeed'],
+                transformed_data['winddirection'],
+                transformed_data['weathercode'],
+            ))
 
+            conn.commit()
+            cursor.close()
 
-
-
+        # DAG Workflow -  ETL Pipeline
+        weather_data = extract_weather_data()
+        transformed_data = transform_weather_data(weather_data)
+        load_weather_data(transformed_data)
